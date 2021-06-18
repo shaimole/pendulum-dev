52d125e238483f9ceefa13250c925186fd087b26
shaimole <57011853+shaimole@users.noreply.github.com>
2021-06-18 19:12:06 +0200

    added camera movements for zoomin updated node with another version

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,45 @@
+<template>
+  <span class="pulse"> </span>
+</template>
+<script>
+export default {
+  name: 'LoadingSpinner'
+}
+</script>
+<style scoped>
+.pulse {
+  display: block;
+  width: 22px;
+  height: 22px;
+  border-radius: 50%;
+  background: #fcd097;
+  box-shadow: 0 0 0 rgba(204, 169, 44, 0.4);
+  animation: pulse 1s infinite;
+}
+
+@-webkit-keyframes pulse {
+  0% {
+    -webkit-box-shadow: 0 0 0 0 #fee6c8;
+  }
+  50% {
+    -webkit-box-shadow: 0 0 0 5px #fee6c8;
+  }
+  100% {
+    -webkit-box-shadow: 0 0 0 0 #fee6c8;
+  }
+}
+@keyframes pulse {
+  0% {
+    -moz-box-shadow: 0 0 0 0 #fee6c8;
+    box-shadow: 0 0 0 0 #fee6c8;
+  }
+  50% {
+    -moz-box-shadow: 0 0 0 5px #fee6c8;
+    box-shadow: 0 0 0 5px #fee6c8;
+  }
+  100% {
+    -moz-box-shadow: 0 0 0 0 #fee6c8;
+    box-shadow: 0 0 0 0 #fee6c8;
+  }
+}
+</style>
--- src/components/SkillTree.vue
+++ src/components/SkillTree.vue
@@ -2,10 +2,10 @@
   <div
     id="container"
     class="h-full w-full flex-1 relative"
-    @scroll="changeCam()"
+    @wheel="zoom($event)"
     @click="clickTest()"
   >
-    <div :show="!isLoadingFinished" class="text-white loading">Loading</div>
+    <LoadingSpinner class="loading" />
   </div>
 </template>
 
@@ -18,6 +18,7 @@ import skyboxDown from '../assets/skybox/bottom.png'
 import skyboxLeft from '../assets/skybox/left.png'
 import skyboxRight from '../assets/skybox/right.png'
 import robotoRegular from '../assets/fonts/Roboto_Regular.json'
+const amount = 30
 export default {
   name: 'SkillTree',
   data () {
@@ -25,6 +26,9 @@ export default {
       camera: null,
       isLoadingFinished: false,
       currentRotation: 0,
+      zMove: 0,
+      xRotation: 0,
+      lines: [],
       window: window,
       scene: null,
       mousedown: false,
@@ -61,7 +65,8 @@ export default {
       const far = 100
       this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
       this.camera.position.z = 0
-      this.camera.position.y = 5
+      this.camera.position.y = 6
+      this.camera.rotation.order = 'YXZ'
     },
     initFont () {
       const fontLoader = new THREE.FontLoader()
@@ -81,14 +86,16 @@ export default {
           skyboxLeft,
           skyboxRight
         ],
-        () => (this.isInit = true),
+        () => {
+          setTimeout(() => (this.isLoadingFinished = true), 500)
+        },
         error => console.log(error)
       )
 
       this.scene.background = texture
     },
     initContent () {
-      for (let i = 0; i !== 10; i++) {
+      for (let i = 0; i !== amount; i++) {
         this.addTree(0, 0)
       }
     },
@@ -100,6 +107,7 @@ export default {
     },
     initControls () {
       const container = document.getElementById('container')
+      container.addEventListener('scroll', e => console.log('jo'))
       container.addEventListener(
         'touchmove',
         function (event) {
@@ -127,13 +135,56 @@ export default {
       this.initControls()
     },
     onDragDefault ({ movementX }) {
+      if (this.rotation) {
+        return
+      }
       if (movementX > 0) {
-        this.rotation = (Math.PI * 2) / 10
+        this.rotation = (Math.PI * 2) / amount
         return
       }
       if (movementX < 0) {
-        this.rotation = -((Math.PI * 2) / 10)
+        this.rotation = -((Math.PI * 2) / amount)
       }
+    },
+    zoom (event) {
+      if (this.zMove !== 0) {
+        return
+      }
+      console.log('fire')
+      if (event.deltaY < 0) {
+        this.zMove = amount * 2 - 8
+        this.xRotation = 1
+      } else {
+        this.zMove = -amount * 2 + 8
+        this.xRotation = -1
+      }
+    },
+    createGeometry (radius) {
+      const geometry = new THREE.BufferGeometry()
+      const vertices = []
+
+      const vertex = new THREE.Vector3()
+
+      for (let i = 0; i < 666; i++) {
+        vertex.x = Math.random() * 2 - 1
+        vertex.y = Math.random() * 2 - 1
+        vertex.z = Math.random() * 2 - 1
+        vertex.normalize()
+        vertex.multiplyScalar(radius)
+
+        vertices.push(vertex.x, vertex.y, vertex.z)
+
+        vertex.multiplyScalar(Math.random() * 0.09 + 1)
+
+        vertices.push(vertex.x, vertex.y, vertex.z)
+      }
+
+      geometry.setAttribute(
+        'position',
+        new THREE.Float32BufferAttribute(vertices, 3)
+      )
+
+      return geometry
     },
     addTree (center, distance) {
       const group = new THREE.Group()
@@ -148,13 +199,28 @@ export default {
         new THREE.Vector3(center, 12, distance)
       ]
       for (const index in positions) {
-        const geometry = new THREE.BoxGeometry(1, 1, 1)
-        const material = new THREE.MeshNormalMaterial()
-        this.mesh = new THREE.Mesh(geometry, material)
-        group.add(this.mesh)
-        this.mesh.position.copy(positions[index])
+        for (const sphere of [
+          { radius: 1, color: 0xcc081d, opacity: 0.75, scale: 0.25 },
+          { radius: 1.5, color: 0xfd6e73, opacity: 0.5, scale: 0.25 },
+          { radius: 4, color: 0x986303, opacity: 0.25, scale: 0.1 }
+        ]) {
+          const line = new THREE.LineSegments(
+            this.createGeometry(sphere.radius),
+            new THREE.LineBasicMaterial({
+              color: sphere.color,
+              opacity: sphere.opacity
+            })
+          )
+          line.scale.x = line.scale.y = line.scale.z = sphere.scale
+          line.userData.originalScale = sphere.scale
+          line.rotation.y = Math.random() * Math.PI
+          line.updateMatrix()
+          group.add(line)
+          line.position.copy(positions[index])
+          this.lines.push(line)
+        }
       }
-      const material = new THREE.LineBasicMaterial({ color: 0x0000ff })
+      const material = new THREE.LineBasicMaterial({ color: 0xcc081d })
 
       const points = []
       points.push(new THREE.Vector3(center, 0, distance))
@@ -184,27 +250,27 @@ export default {
       const line3 = new THREE.Line(geometry3, material)
 
       group.add(line3)
-      var textGeo = new THREE.TextGeometry('X', {
-        font: this.font,
-        size: 2,
-        height: 1,
-        curveSegments: 1,
-        bevelEnabled: true,
-        bevelThickness: 0.02,
-        bevelSize: 0.02,
-        bevelOffset: 0,
-        bevelSegments: 5
-      })
-      const text = new THREE.Mesh(
-        textGeo,
-        new THREE.LineBasicMaterial({ color: 0xffffff })
-      )
-      text.position.copy(new THREE.Vector3(center, -5, distance))
-      text.geometry.center()
-      group.add(text)
-      group.rotation.y += this.currentRotation
-      group.translateZ(-50)
-      this.currentRotation += (Math.PI * 2) / 10
+      // var textGeo = new THREE.TextGeometry('X', {
+      //   font: this.font,
+      //   size: 2,
+      //   height: 1,
+      //   curveSegments: 1,
+      //   bevelEnabled: true,
+      //   bevelThickness: 0.02,
+      //   bevelSize: 0.02,
+      //   bevelOffset: 0,
+      //   bevelSegments: 5
+      // })
+      // const text = new THREE.Mesh(
+      //   textGeo,
+      //   new THREE.LineBasicMaterial({ color: 0xffffff })
+      // )
+      // text.position.copy(new THREE.Vector3(center, -5, distance))
+      // text.geometry.center()
+      // group.add(text)
+      group.rotation.y = this.currentRotation
+      group.translateZ(-amount * 2)
+      this.currentRotation += (Math.PI * 2) / amount
     },
     calcPos (node, pos) {
       for (const child in node.children) {
@@ -225,47 +291,74 @@ export default {
     },
     animate: function () {
       requestAnimationFrame(this.animate)
-      if (!this.isInit) {
+      for (const line of this.lines) {
+        line.rotation.y += 0.01
+      }
+      if (!this.isLoadingFinished) {
         return
       }
+      this.spinCamera()
+      this.zoomCamera()
+      this.renderer.render(this.scene, this.camera)
+    },
+    zoomCamera () {
+      if (this.zMove > 0 || (this.zMove && !this.xRotation)) {
+        this.moveCameraZ()
+        return
+      }
+      if (this.xRotation !== 0) {
+        console.log(this.camera)
+        this.rotateCamera()
+      }
+    },
+    rotateCamera () {
+      const maxCameraRotation = 0.15
+      let xRotationThisFrame = maxCameraRotation
+      if (Math.abs(this.xRotation) < maxCameraRotation) {
+        xRotationThisFrame = Math.abs(this.xRotation)
+      }
+      if (this.xRotation < 0) {
+        xRotationThisFrame = -xRotationThisFrame
+      }
+      this.xRotation -= xRotationThisFrame
+      this.camera.rotation.x += xRotationThisFrame
+      this.camera.position.y -= xRotationThisFrame * 10
+    },
+    moveCameraZ () {
+      const maxCameraZMove = 0.1 * amount
+      let zMoveThisFrame = maxCameraZMove
+      if (Math.abs(this.zMove) < maxCameraZMove) {
+        zMoveThisFrame = Math.abs(this.zMove)
+      }
+      if (this.zMove < 0) {
+        zMoveThisFrame = -zMoveThisFrame
+      }
+      this.zMove -= zMoveThisFrame
+      this.camera.translateZ(-zMoveThisFrame)
+    },
+    spinCamera () {
+      const maxCameraHorizontalMove = 0.02
       let roationThisFrame
       if (this.rotation > 0) {
-        roationThisFrame = 0.01
-        if (this.rotation < 0.01) {
+        roationThisFrame = maxCameraHorizontalMove
+        if (this.rotation < maxCameraHorizontalMove) {
           roationThisFrame = this.rotation
         }
         this.rotation -= roationThisFrame
         this.camera.rotation.y += roationThisFrame
       } else if (this.rotation < 0) {
-        roationThisFrame = -0.01
-        if (Math.abs(this.rotation) < 0.01) {
+        roationThisFrame = -maxCameraHorizontalMove
+        if (Math.abs(this.rotation) < maxCameraHorizontalMove) {
           roationThisFrame = this.rotation
         }
         this.rotation -= roationThisFrame
         this.camera.rotation.y += roationThisFrame
       }
-
-      this.renderer.render(this.scene, this.camera)
-    },
-    addCube (pos) {
-      const geometry = new THREE.BoxGeometry(1, 1, 1)
-      const material = new THREE.MeshNormalMaterial()
-      this.mesh = new THREE.Mesh(geometry, material)
-      this.scene.add(this.mesh)
-      this.mesh.position.copy(pos)
     }
   },
   mounted () {
     this.init()
     this.animate()
-  },
-  computed: {
-    getCanvasSize () {
-      return {
-        height: 1000,
-        width: 1000
-      }
-    }
   }
 }
 </script>
