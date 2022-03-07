e8c93c8c53a9fab3e0c18e48c00ed05a892785ff
shaimole <57011853+shaimole@users.noreply.github.com>
2022-03-07 17:13:43 +0100

    test for later refactoring

---
+++ tests/data/attackStats.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 'war',
+    label: 'Kriegswaffen',
+    mod: -4,
+    parts: ['KK', 'GS'],
+    value: 0
+  },
+  {
+    id: 'finesse',
+    label: 'Finessewaffen',
+    mod: -4,
+    parts: ['GS', 'WN'],
+    value: 0
+  },
+  {
+    id: 'range',
+    label: 'Fernkampf',
+    mod: -4,
+    parts: ['GS', 'WN'],
+    value: 0
+  },
+  {
+    id: 'thrown',
+    label: 'Schleuderwaffen',
+    mod: -4,
+    parts: ['GS', 'WN'],
+    altparts: ['KK', 'GS'],
+    value: 0
+  },
+  {
+    id: 'unarmed',
+    label: 'Unbewaffnet',
+    mod: -4,
+    parts: ['KK', 'GS'],
+    value: 0
+  }
+]
--- tests/unit/data/attackStats.spec.js
+++ tests/unit/data/attackStats.spec.js
@@ -1,8 +1,12 @@
 import attackStats from '@/data/attackStats'
+import attackStatsDefintions from '../../data/attackStats'
 import { expect } from 'chai'
 
 describe('Get Attack Stats Definitions', () => {
   it('should be an array', () => {
     expect(attackStats).to.be.an('array')
+  })
+  it('should contain the correct definitions', () => {
+    expect(attackStats).to.eql(attackStatsDefintions)
   })
 })
